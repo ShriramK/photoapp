22b619c965df6f6e810cac3794417e9745b92110
Shriram Kunchanapalli <kshriram18@gmail.com>
2013-07-20 22:44:04 +0530

    Adds photo app search, filter and sort functionality

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('photo.views',
+	url(r"^search/$", "search"),
 	url(r"^update/$", "update"),
 	url(r"^image/(\d+)/$", "image"),
 	url(r"^(\d+)/(full|thumbnails|edit)/$", "album"),
--- views.py
+++ views.py
@@ -5,12 +5,13 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.db.models import Q
 from django.forms import ModelForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 #from mysite.settings import MEDIA_ROOT
 from django.conf import settings
-from photo.models import Album, Image, Tag
+from photo.models import Album, Image, Tag, User
 from string import join
 
 
@@ -124,3 +125,134 @@ def update(request):
 
 	#removed dict(media_url) as an argument to HttpResponseRedirect
 	return HttpResponseRedirect(request.META["HTTP_REFERER"])
+
+@login_required
+def search(request):
+	"""Search, filter, sort images."""
+	try:
+		page = int(request.GET.get("page", '1'))
+	except ValueError:
+		page = 1
+
+	p = request.POST
+	images = defaultdict(dict)
+
+	#init parameters
+	parameters = {}
+	keys = "title filename rating_from rating_to width_from width_to height_from height_to tags view"\
+		" user sort asc_desc"
+	keys = keys.split()
+	for k in keys:
+		parameters[k] = ''
+	parameters["album"] = []
+
+	#create dictionary of properties for each image and a dict of search/filter parameters
+	for k, v in p.items():
+		if k == "album":
+			parameters[k] = [int(x) for x in p.getlist(k)]
+		elif k == "user":
+			if v != "all":
+				v = int(v)
+			parameters[k] = v
+		elif k in parameters:
+			parameters[k] = v
+		elif k.startswith("title") or k.startswith("rating") or k.startswith("tags"):
+			k, pk = k.split('-')
+			images[pk][k] = v
+		elif k.startswith("album"):
+			pk = k.split('-')[1]
+			images[pk]["albums"] = p.getlist(k)
+
+	# save or restore parameters from session
+	if page != 1 and "parameters" in request.session:
+		parameters = request.session["parameters"]
+	else:
+		request.session["parameters"] = parameters
+
+	results = update_and_filter(images, parameters)
+
+	# make paginator
+	paginator = Paginator(results, 20)
+	try:
+		results = paginator.page(page)
+	except (InvalidPage, EmptyPage):
+		results = paginator.page(paginator.num_pages)
+		#request = paginator.page(paginator.num_pages)
+
+	# add list of tags as string and list of album names to each image object
+	for img in results.object_list:
+		tags = [x[1] for x in img.tags.values_list()]
+		img.tag_lst = join(tags, ', ')
+		img.album_lst = [x[1] for x in img.albums.values_list()]
+	
+	d = dict(results=results, user=request.user, albums=Album.objects.all(), prm=parameters,
+			 users=User.objects.all(), media_url=settings.MEDIA_URL)
+	d.update(csrf(request))
+	return render_to_response("photo/search.html", d)
+
+def update_and_filter(images, p):
+	"""Update image data if changed, filter results through parameters and return results list."""
+	# process properties, assign to image objects and save
+	for k, d in images.items():
+		image = Image.objects.get(pk=k)
+		image.title = d["title"]
+		image.rating = int(d["rating"])
+
+		# tags - assign or create if a new tag!
+		tags = d["tags"].split()
+		lst = []
+		for t in tags:
+			if t:
+				lst.append(Tag.objects.get_or_create(tag=t)[0])
+		image.tags = lst
+
+		if "albums" in d:
+			image.albums = d["albums"]
+		image.save()
+
+	# filter results by parameters
+	results = Image.objects.all()
+	if p["title"]:
+		results = results.filter(title__icontains=p["title"])
+	if p["filename"]:
+		results = results.filter(image__icontains=p["filename"])
+	if p["rating_from"]:
+		results = results.filter(rating__gte=int(p["rating_from"]))
+	if p["rating_to"]:
+		results = results.filter(rating__lte=int(p["rating_to"]))
+	if p["width_from"]:
+		results = results.filter(width__gte=int(p["width_from"]))
+	if p["width_to"]:
+		results = results.filter(width__lte=int(p["width_to"]))
+	if p["height_from"]:
+		results = results.filter(height__gte=int(p["height_from"]))
+	if p["height_to"]:
+		results = results.filter(height__lte=int(p["height_to"]))
+
+	# sort and filter results by parameters
+	order = "created"
+	if p["sort"]: 
+		order = p["sort"]
+	if p["asc_desc"] == "desc":
+		order = '-' + order
+
+	results = Image.objects.all().order_by(order)
+	if p["user"] and p["user"] != "all":
+		results = results.filter(user__pk=int(p["user"]))
+
+
+	if p["tags"]:
+		tags = p["tags"].split(', ')
+		lst = []
+		for t in tags:
+			if t:
+				results = results.filter(tags=Tag.objects.get(tag=t))
+
+	if p["album"]:
+		lst = p["album"]
+		or_query = Q(albums=lst[0])
+		for album in lst[1:]:
+			or_query = or_query | Q(albums=album)
+		results = results.filter(or_query).distinct()
+
+	return results
